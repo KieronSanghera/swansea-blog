0339fcac132d5f206ef088e87af923ed1ad2355b
KieronSanghera <74434574+KieronSanghera@users.noreply.github.com>
2021-11-04 20:28:15 +0000

    Added post relationship (many comments on one post)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,9 @@ class Post extends Model
     {
         return $this->belongsTo(Admin::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
