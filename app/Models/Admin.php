c31149cf4ee24bfd38a77ee8b073984b12c09a78
KieronSanghera <74434574+KieronSanghera@users.noreply.github.com>
2021-11-04 18:41:54 +0000

    Added one to many relation to admins posts

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,5 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
-    
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
