397bea004eeb056904ce5991246e595adf8dfc3f
KieronSanghera <74434574+KieronSanghera@users.noreply.github.com>
2021-12-04 19:22:48 +0000

    added controller for specified Post

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,6 @@ Route::get('/post/{postTitle?}', function ($postTitle=null) {
 });
 
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
 
 require __DIR__.'/auth.php';
