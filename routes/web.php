0bcb90a7f7b6a8d451d87194ae421f1c2743c3c0
KieronSanghera <74434574+KieronSanghera@users.noreply.github.com>
2021-12-13 21:31:44 +0000

    Re-added the nav bar that was removed in previous commits

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,8 @@ Route::get('/post/{postTitle?}', function ($postTitle=null) {
     return view('post', ['postTitle'=>$postTitle]);
 }); 
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index')->middleware((['auth']));
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create')->middleware(['auth']);
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 
