4065fd7c37472daab87e14127a9ae5c32cf7cb29
KieronSanghera <74434574+KieronSanghera@users.noreply.github.com>
2021-12-15 18:42:47 +0000

    Added timestamps to admin and student

--- database/migrations/2021_11_03_202643_create_admins_table.php
+++ database/migrations/2021_11_03_202643_create_admins_table.php
@@ -17,6 +17,7 @@ class CreateAdminsTable extends Migration
             $table->id();
             $table->string('name');
             $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')
                 ->onDelete('cascade')->onUpdate('cascade');
         });
