e4dcca3b69ce462293c1a89ccebfbc1d90594e66
KieronSanghera <74434574+KieronSanghera@users.noreply.github.com>
2021-12-15 23:43:55 +0000

    Design for post page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,11 +3,13 @@
 @section('title')
 
 @section('content')
-
-    <h1>Post - {{$post->title}}</h1>
-    <p>created by: {{$post->admin->name}}</p>
-    <p>{{$post->body}}</p>
+    <figure class="m-10 flex-none bg-white rounded-3xl p-8 md:p-0">
+        <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+            <h1 class="text-3xl my-6">{{ $post->title }}</h1>
+            <p>{{ $post->body }}</p>
+            <p class="text-blue-700 text-right" >created by: {{ $post->admin->name }}</p>
+        </div>
+    </figure>
     <button method='DELETE' href='{{ route('posts.destroy') }}''>Delete Post</button>
-    
 
 @endsection
